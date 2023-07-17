505abdc62ea8aaa92bf2890187ccae989784ad5b
Sergiyan92 <sergiyan@ukr.net>
2023-07-17 12:15:53 +0300

    code reduction

---
+++ controllers/contacts.js
@@ -0,0 +1,47 @@
+const contacts = require("../models/contacts");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const addContacts = async (req, res, next) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContacts = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const deleteContacts = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  addContacts: ctrlWrapper(addContacts),
+  updateContacts: ctrlWrapper(updateContacts),
+  deleteContacts: ctrlWrapper(deleteContacts),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,77 +1,18 @@
 const express = require("express");
-const Joi = require("joi");
-const contacts = require("../../models/contacts");
-const { HttpError } = require("../../helpers");
-
 const router = express.Router();
-const addJoiSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
-router.get("/", async (req, res) => {
-  try {
-    const result = await contacts.listContacts();
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schema = require("../../schemas/contacts");
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.getContactById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", ctrl.getAll);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addJoiSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing required name field");
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrl.getById);
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = addJoiSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing required name field");
-    }
-    const { id } = req.params;
-    const result = await contacts.updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validateBody(schema.addJoiSchema), ctrl.addContacts);
+
+router.put("/:id", validateBody(schema.addJoiSchema), ctrl.updateContacts);
+
+router.delete("/:id", ctrl.deleteContacts);
 
 module.exports = router;
