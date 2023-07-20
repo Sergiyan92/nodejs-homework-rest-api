111361b823cbf1a141a4b3236038d618ad29fdf0
Sergiyan92 <sergiyan@ukr.net>
2023-07-20 11:55:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Serhii:wienAfbexAvGAo0R@cluster0.dd74dqt.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
