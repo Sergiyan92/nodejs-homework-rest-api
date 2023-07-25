e805677eda9c862c9563dfbf810b557a71f8a256
Sergiyan92 <sergiyan@ukr.net>
2023-07-25 20:06:00 +0300

    create auth

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Set password for user"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  token: String,
+});
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().required(),
+  subscription: Joi.string(),
+});
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const router = express.Router();
-
 const ctrl = require("../../controllers/contacts");
 const { validateBody, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
+const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
