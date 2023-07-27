394c505161f1c29043adefd3624a3b936f8fc456
Sergiyan92 <sergiyan@ukr.net>
2023-07-27 11:53:57 +0300

    create filter

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,12 +3,15 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "", {
-    skip,
-    limit,
-  }).populate("owner", "email subscription");
+  const filter = { owner };
+  if (favorite === "true") {
+    filter.favorite = true;
+  } else if (favorite === "false") {
+    filter.favorite = false;
+  }
+  const result = await Contact.find(filter, "", { skip, limit });
   res.status(200).json(result);
 };
 const getById = async (req, res) => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,17 @@ const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
+
 const router = express.Router();
 
 router.get("/", authenticate, ctrl.getAll);
-
 router.get("/:id", authenticate, isValidId, ctrl.getById);
-
 router.post(
   "/",
   authenticate,
   validateBody(schemas.addJoiSchema),
   ctrl.addContacts
 );
-
 router.put(
   "/:id",
   authenticate,
